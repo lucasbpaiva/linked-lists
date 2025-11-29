52654cec1dce007bf0dbdf001f35a892bf85f385
Lucas Paiva <lpaiva234@gmail.com>
2025-11-28 22:24:45 -0300

    implement prepend/append methods

---
+++ LinkedList.js
@@ -0,0 +1,63 @@
+export class Node {
+    constructor(val, next) {
+        this.val = (val === undefined ? null : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+export class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    prepend(value) {
+        // adds a new node containing value to the start of the list
+        const newNode = new Node(value, this.head);
+        if (this.head === null) this.tail = newNode;
+        this.head = newNode;
+        this.size++;
+    }
+
+    append(value) {
+        // adds a new node containing value to the end of the list
+        const newNode = new Node(value, null);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.size++;
+    }
+
+    at(index) {
+        // returns the node at the given index
+    }
+
+    pop() {
+        // removes the last element from the list
+    }
+
+    contains(value) {
+        // returns whether the passed in value is in the list
+    }
+
+    find(value) {
+        // returns the index of the node containing value, or null if not found
+    }
+
+    toString() {
+        // represents LinkedList object as a string in the format: ( value ) -> ( value ) -> ( value ) -> null
+    }
+
+    insertAt(value, index) {
+        // inserts a new node with the provided value at the given index
+    }
+
+    removeAt(index) {
+        // removes the node at the given index
+    }
+}
